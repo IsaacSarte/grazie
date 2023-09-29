abc2bf85700de7bea59ee93ba06279514921b1dd
Isaac Sarte <sarteisaac09@gmail.com>
2023-09-29 22:29:31 +0800

    install tailwind css

--- src/modules/banner.jsx
+++ src/modules/banner.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 const Banner = () => {
   return (
     <React.Fragment>
-      <div className="">
-        Banner
-      </div>
+      <div className="h-screen flex">
+        <div className="w-1/2">
+            Left Div
+        </div>
+        <div className="w-1/2">
+            Right Div
+        </div>
+    </div>
     </React.Fragment>
   )
 }
