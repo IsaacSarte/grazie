32fcba2e01ce3cd867fbaf04d2981fcfe33000fd
SimonBFornillos <simonbfornillos@gmail.com>
2023-10-09 21:59:18 +0800

    added Prducts page

--- src/modules/exploreProducts.jsx
+++ src/modules/exploreProducts.jsx
@@ -1,5 +1,7 @@
 import DescriptionWithButton from '@/components/description';
 import React from 'react'
+import Image from 'next/image';
+import { products } from "../staticData/products.js";
 
 const ExploreProducts = () => {
   const desc = 'Each bite tells a story of love, passed down through generations.';
@@ -9,15 +11,25 @@ const ExploreProducts = () => {
       <div className="h-screen bg-slate-100">
         <div className="w-[95%] mx-auto">
           <DescriptionWithButton
-            withButton={true} 
+            withButton={true}
             buttonLabel={'Shop Now'}
             description={desc}
             bgColor={'bg-yellow-800'}
             txtColor={'text-white'}
           />
         </div>
-
-        {/* map imported static products data */}
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-14 mt-8 w-[95%] mx-auto">
+          {products.map((product, index) => (
+            <>
+              <div key={index} className="bg-white p-4 rounded shadow-md">
+                <div className="image-container w-full h-72 overflow-hidden">
+                  <Image src={product.image} alt={product.title} className="w-full h-auto" />
+                </div>
+              </div>
+              <h2 className="text-lg font-semibold mt-2">{product.title}</h2>
+            </>
+          ))}
+        </div>
       </div>
     </React.Fragment>
   )
--- src/staticData/products.js
+++ src/staticData/products.js
@@ -1,1 +1,69 @@
-// Create an array of objects containing 'product_title' and 'product_price'
+import cashewNut from "../../public/images/cashewNut.png";
+import cashewCoco from "../../public/images/cashewCoconut.png";
+import cashewDur from "../../public/images/cashewDurian.png";
+import crispMang from "../../public/images/crispyMango.png";
+import crispPine from "../../public/images/crispyPineapple.png"
+import crispBanan from "../../public/images/crispyBanana.png";
+import fdPine from "../../public/images/FDPineapple.jpg";
+import fdBanan from "../../public/images/FDBanana.jpg";
+import fdMangosteen from "../../public/images/FDMangosteen.jpg";
+import fdMango from "../../public/images/FDMango.jpg";
+import mixedNuts from "../../public/images/mixedNutsAndDriedFruits.png";
+import cerealBar from "../../public/images/cerealBar.jpg";
+
+const products = [{
+        title: "Cashew Nut(45g)",
+        image: cashewNut,
+    },
+    {
+        title: "Cashew Nut Coconut(45g)",
+        image: cashewCoco,
+        
+    },
+    {
+        title: "Cashew Nut Durian(45g)",
+        image: cashewDur,
+        
+    },
+    {
+        title: "Crispy Mango(50g)",
+        image: crispMang,
+        
+    },
+    {
+        title: "Crispy Pineapple(50g)",
+        image: crispPine,
+    },
+    {
+        title: "Crispy Banana(50g)",
+        image: crispBanan,
+    },
+    {
+        title: "Freeze Dried Pineapple(50g)",
+        image: fdPine,
+    },
+    {
+        title: "Freeze Dried Banana(50g)",
+        image: fdBanan,
+    },
+    {
+        title: "Freeze Dried Mangosteen(50g)",
+        image: fdMangosteen,
+    },
+    {
+        title: "Freeze Dried Mango(50g)",
+        image: fdMango,
+    },
+    {
+        title: "Mixed Grain and Dried Fruits(500g)",
+        image: mixedNuts,
+    },
+    {
+        title: "Multigrain/Cereal Bar",
+        image: cerealBar,
+    },
+];
+
+export{
+    products
+};
