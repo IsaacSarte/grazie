ca33e0967e973b1ffb32f67e7b3c896cd7f32538
Isaac Sarte <sarteisaac09@gmail.com>
2023-10-11 22:00:16 +0800

    added animations

--- src/modules/exploreProducts.jsx
+++ src/modules/exploreProducts.jsx
@@ -1,10 +1,46 @@
+"use client";
+import React, { useEffect } from 'react';
 import DescriptionWithButton from '@/components/description';
-import React from 'react'
 import Image from 'next/image';
 import { products } from "../staticData/products.js";
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from 'framer-motion';
 
 const ExploreProducts = () => {
   const desc = 'Each bite tells a story of love, passed down through generations.';
+
+  const { ref, inView } = useInView({
+    threshold: 0.1
+  });
+
+  const fade = useAnimation();
+  const animationLeft = useAnimation();
+  const animationRight = useAnimation();
+  const animationDown = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+        fade.start({
+            opacity: 1
+        });
+        animationDown.start({
+            y: 0,
+            transition: {
+                type: 'spring', duration: 1.25, bounce: 0.2
+            }
+        });
+    }
+
+    if (!inView) {
+        fade.start({
+            opacity: 0
+        });
+        animationDown.start({
+            y: '-100px'
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
 
   return (
     <React.Fragment>
@@ -18,17 +54,20 @@ const ExploreProducts = () => {
             txtColor={'text-white'}
           />
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-14 mt-8 w-[95%] mx-auto">
-          {products?.map((product, index) => (
+        <div ref={ref} className="grid grid-cols-2 md:grid-cols-3 gap-14 mt-8 w-[95%] mx-auto">
+          {products?.map((product) => (
             <>
-              <div key={`prod-${index}`}>
-                <div className="bg-white p-4 h-72 overflow-hidden">
+              <div>
+                <motion.div
+                    animate={animationDown}
+                    className="bg-white p-4 h-72 overflow-hidden"
+                >
                     <Image
                         src={product?.image} 
                         alt={product?.title}
                     />
-                </div>
-                <h2 className="text-lg font-semibold mt-4">{product?.title}</h2>
+                </motion.div>
+                <motion.h2 animate={fade} className="text-lg font-semibold mt-4">{product?.title}</motion.h2>
               </div>
             </>
           ))}
--- src/modules/missionVision.jsx
+++ src/modules/missionVision.jsx
@@ -1,10 +1,52 @@
 "use client";
-import React from 'react';
+import React, { useEffect } from 'react';
 import DescriptionWithButton from '@/components/description';
-import { AnimatePresence, motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from 'framer-motion';
 
 const MissionVision = () => {
   const desc = "From Granny secrets to modern delights, savor the essence of tradition of our homemade foods.";
+
+  const { ref, inView } = useInView({
+    threshold: 0.25
+  });
+
+  const fade = useAnimation();
+  const animationLeft = useAnimation();
+  const animationRight = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+        fade.start({
+            opacity: 1
+        });
+        animationLeft.start({
+            x: 0,
+            transition: {
+                type: 'spring', duration: 1.25, bounce: 0.15
+            }
+        });
+        animationRight.start({
+            x: 0,
+            transition: {
+                type: 'spring', duration: 1.25, bounce: 0.15
+            }
+        });
+    }
+
+    if (!inView) {
+        fade.start({
+            opacity: 0
+        });
+        animationLeft.start({
+            x: '-100px',
+        });
+        animationRight.start({
+            x: '100px'
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
 
   return (
     <React.Fragment>
@@ -20,33 +62,55 @@ const MissionVision = () => {
           />
         </div>
 
-        <AnimatePresence>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.25 }}
+        <div
+            ref={ref}
             className="mt-4 md:h-[70vh] flex flex-col w-[90%] mx-auto md:w-full md:flex-row justify-center gap-8"
-          >
+        >
             {/* mission */}
-            <div className="relative md:w-[25%] bg-slate-100 flex flex-col">
-              <div className="product-three bg-cover bg-center h-[10vh] md:h-[35vh]" />
-              <div className="p-4">
-                <span className="text-2xl md:text-4xl">Mission</span>
-                <hr className="h-1 w-1/2 bg-black" />
-                <p className="mt-2 text-sm md:text-base">We strive to deliver timeless and exceptional food experiences that transcend generations, crafting a legacy of joy and well-being. Through our commitment to quality, innovation, and health, we aim to delight taste buds across ages, fostering a community that cherishes the art of nourishing both body and soul.</p>
-              </div>
-            </div>
+            <motion.div
+                animate={fade}
+                className="relative md:w-[25%] bg-slate-100 flex flex-col"
+            >
+                <div className="product-three bg-cover bg-center h-[10vh] md:h-[35vh]" />
+                <div className="p-4" ref={ref}>
+                    <motion.span
+                        animate={fade}
+                        className="text-2xl md:text-4xl"
+                    >
+                        Mission
+                    </motion.span>
+                    <hr className="h-1 w-1/2 bg-black" />
+                    <motion.p
+                        animate={animationLeft}
+                        className="mt-2 text-sm md:text-base"
+                    >
+                        We strive to deliver timeless and exceptional food experiences that transcend generations, crafting a legacy of joy and well-being. Through our commitment to quality, innovation, and health, we aim to delight taste buds across ages, fostering a community that cherishes the art of nourishing both body and soul.
+                    </motion.p>
+                </div>
+            </motion.div>
             {/* vision */}
-            <div className="relative md:w-[25%] mb-16 md:mb-0 bg-slate-100 flex flex-col">
-              <div className="product-four bg-cover bg-center h-[10vh] md:h-[35vh]" />
-              <div className="p-4">
-                <span className="text-2xl md:text-4xl">Vision</span>
-                <hr className="h-1 w-1/2 bg-black" />
-                <p className="mt-2 text-sm md:text-base">To be the heart of homes worldwide, where our diverse range of special foods and healthy snacks becomes synonymous with moments of happiness and vitality. We envision a future where every generation finds comfort, pleasure, and wellness in our products, creating a lasting tradition of savoring life&apos;s moments with our timeless delights.</p>
-              </div>
-            </div>
-          </motion.div>
-        </AnimatePresence>
+            <motion.div 
+                animate={fade}
+                className="relative md:w-[25%] mb-16 md:mb-0 bg-slate-100 flex flex-col"
+            >
+                <div className="product-four bg-cover bg-center h-[10vh] md:h-[35vh]" />
+                <div className="p-4" ref={ref}>
+                    <motion.span
+                        animate={fade}
+                        className="text-2xl md:text-4xl"
+                    >
+                        Vision
+                    </motion.span>
+                    <hr className="h-1 w-1/2 bg-black" />
+                    <motion.p
+                        animate={animationRight}
+                        className="mt-2 text-sm md:text-base"
+                    >
+                        To be the heart of homes worldwide, where our diverse range of special foods and healthy snacks becomes synonymous with moments of happiness and vitality. We envision a future where every generation finds comfort, pleasure, and wellness in our products, creating a lasting tradition of savoring life&apos;s moments with our timeless delights.
+                    </motion.p>
+                </div>
+            </motion.div>
+        </div>
       </div>
     </React.Fragment>
   );
